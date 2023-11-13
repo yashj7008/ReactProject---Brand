5aef1003fd2ae98dab45c3ef39110c8c573c9e37
yashj7008 <yashj7008@gmail.com>
2023-11-13 16:06:00 +0530

    first commit

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,38 @@
+
+const Hero = () => {
+    return (
+       
+        <div className="hero-body container">
+            <div className="hero-content">
+                <div className="heading">
+                    <p>YOUR FEET DESERVE THE BEST</p>
+                </div>
+                <div className="para">
+                    <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO <br/>
+                     HELP YOU WITH OUR SHOES.YOUR FEET DESERVE <br/>
+                     THE BEST AND WE’RE HERE TO HELP YOU WITH OUR <br/>
+                      SHOES.</p>
+                </div>
+                <div className="buttons">
+                    <button className="shop">Shop Now</button>
+                    <button className="cat">Category</button>
+                </div>
+                <div className="sentence">
+                    <p>Also available on </p>
+                </div>
+                <div className="small-logo">
+                    <img src="/images/flipkart.png" alt="logo" />
+                    <img src="/images/amazon.png" alt="logo" />
+                </div>
+                
+            </div>
+            <div className="hero-img">
+                <img src="/images/shoe_image.png" alt="" />
+            </div>
+        </div>
+       
+        
+    ) 
+}
+
+export default Hero;
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,22 @@
+
+const Nav = () => {
+    return (
+       
+         <div className="navigation-body container">
+            <div className="nike-img">
+                <img src="/images/brand_logo.png" alt="logo" />
+            </div>
+            <ul>
+                <li href = "#" >MENU</li>
+                <li href = "#" >LOCATION</li>
+                <li href = "#" >ABOUT</li>
+                <li href = "#" >CONTACT</li>
+            </ul>
+            <button>Login</button>
+        </div>
+       
+        
+    )
+}
+
+export default Nav;
